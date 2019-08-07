576c5cd5e4f7b59e29e56a855c1f239a94bc2109
Iman Tumorang <iman.tumorang@gmail.com>
2019-08-07 13:46:58 +0700

    feat(interface): add example of implementations of interface

--- main.go
+++ main.go
@@ -2,13 +2,61 @@ package main
 
 import "fmt"
 
-func main() {
-	number := 2
-	Cetak(number)
-	kalimat := "Hello World!"
-	Cetak(kalimat)
+type LivingThing interface {
+	Run(length int) string
 }
 
-func Cetak(item interface{}) {
-	fmt.Println("Item: ", item)
+// Define interface
+type Human interface {
+	LivingThing
+	Cry() string
+}
+
+type Student struct {
+	Name string
+}
+
+func (s Student) Run(length int) string {
+	return fmt.Sprintf("%s run for %d meters", s.Name, length)
+}
+
+func (s Student) Cry() string {
+	return fmt.Sprintf("%s cry like a crazy", s.Name)
+}
+
+type Dog struct {
+	Name string
+}
+
+func (d Dog) Run(length int) string {
+	return fmt.Sprintf("%s the Dog, run for %d meters", d.Name, length)
+}
+
+func main() {
+
+	studentJohn := Student{
+		Name: "John",
+	}
+
+	MakeCry(studentJohn)
+	MakeHumanRun(studentJohn, 20)
+
+	hachiko := Dog{
+		Name: "Hachiko",
+	}
+
+	MakeRun(hachiko, 10)
+	MakeCry(hachiko) // This will throw error on compile
+}
+
+func MakeRun(o LivingThing, length int) {
+	fmt.Println(o.Run(length))
+}
+
+func MakeHumanRun(human Human, length int) {
+	fmt.Println(human.Run(length))
+}
+
+func MakeCry(human Human) {
+	fmt.Println(human.Cry())
 }
